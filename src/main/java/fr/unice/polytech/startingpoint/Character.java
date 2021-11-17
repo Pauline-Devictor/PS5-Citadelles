a36b9d109fbd326e8fe2db65996aebfaa815f12c
Pauline-Devictor <pauline.devictor@etu.unice.fr>
2021-11-17 17:31:57 +0100

    isAvailable #6

--- src/main/java/fr/unice/polytech/startingpoint/Character.java
+++ src/main/java/fr/unice/polytech/startingpoint/Character.java
@@ -1,9 +1,15 @@
 package fr.unice.polytech.startingpoint;
 
+import java.util.List;
+
 public abstract class Character {
      protected int order;
+     protected boolean taken;
 
      int getOrder() {
         return order;
+    }
+    boolean isAvailable(){
+         return taken;
     }
 }
