735bc01dfb8d041883a54b19f3a39a85c31c7f04
Pauline-Devictor <pauline.devictor@etu.unice.fr>
2021-11-17 23:25:48 +0100

    Tests getOrder et getOrder de Thief #6

--- src/test/java/fr/unice/polytech/startingpoint/TestCharacter.java
+++ src/test/java/fr/unice/polytech/startingpoint/TestCharacter.java
@@ -1,8 +1,6 @@
 package fr.unice.polytech.startingpoint;
 
-import fr.unice.polytech.startingpoint.Characters.Assassin;
-import fr.unice.polytech.startingpoint.Characters.King;
-import fr.unice.polytech.startingpoint.Characters.Condottiere;
+import fr.unice.polytech.startingpoint.Characters.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,7 +10,12 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TestCharacter {
 
    Assassin assassin = new Assassin();
+   Thief thief = new Thief();
+   Magician magician = new Magician();
    King king = new King();
+   Bishop bishop = new Bishop();
+   Merchant merchant = new Merchant();
+   Architect architect = new Architect();
    Condottiere condottiere = new Condottiere();
 
     @BeforeEach
@@ -24,8 +27,28 @@ public class TestCharacter {
         assertEquals(1,assassin.getOrder());
     }
     @Test
+    void thiefGetOrder(){
+        assertEquals(2,thief.getOrder());
+    }
+    @Test
+    void magicianGetOrder(){
+        assertEquals(3,magician.getOrder());
+    }
+    @Test
     void kingGetOrder(){
         assertEquals(4,king.getOrder());
+    }
+    @Test
+    void bishopGetOrder(){
+        assertEquals(5,bishop.getOrder());
+    }
+    @Test
+    void merchantGetOrder(){
+        assertEquals(6,merchant.getOrder());
+    }
+    @Test
+    void architectGetOrder(){
+        assertEquals(7,architect.getOrder());
     }
     @Test
     void condottiereGetOrder(){
