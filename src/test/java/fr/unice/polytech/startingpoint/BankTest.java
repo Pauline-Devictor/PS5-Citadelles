29c4c7f50cb50d537e1d37b04c5e0880720a8d5e
AmbreCorreia <74357454+AmbreCorreia@users.noreply.github.com>
2021-11-19 18:38:33 +0100

    BankTest #5

---
+++ src/test/java/fr/unice/polytech/startingpoint/BankTest.java
@@ -0,0 +1,25 @@
+package fr.unice.polytech.startingpoint;
+
+import java.lang.Exception.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BankTest {
+    @Test
+    void withDrawGold1(){
+        Bank b = new Bank(30);
+        assertEquals(3, b.withdrawGold(3));
+    }
+
+    @Test
+    void withDrawGold2(){
+        Bank b = new Bank(30);
+        assertEquals(0, b.withdrawGold(32));
+    }
+
+    @Test
+    void refundGold1(){
+        Bank b = new Bank(30);
+        assertThrows(RuntimeException.class, ()-> b.refundGold(3));
+    }
+}
