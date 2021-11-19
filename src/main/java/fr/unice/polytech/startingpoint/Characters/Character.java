8751aef29a455207f53ae62058869602bbaa46d5
Pauline-Devictor <pauline.devictor@etu.unice.fr>
2021-11-19 17:50:52 +0100

    #6

--- src/main/java/fr/unice/polytech/startingpoint/Characters/Character.java
+++ src/main/java/fr/unice/polytech/startingpoint/Characters/Character.java
@@ -1,19 +1,24 @@
 package fr.unice.polytech.startingpoint.Characters;
 
+import fr.unice.polytech.startingpoint.Player;
+
 import java.util.List;
 
 public abstract class Character {
     protected int order;
     protected boolean available;
     protected String name;
+    protected Player player;
 
     public int getOrder() {
         return order;
     }
     public boolean isAvailable(){return available;}
     public String getName(){return name;}
+    public Player getPlayer(){return player;}
     public void isTaken(){available=false;}//un bot prend la carte
     public void setFree(){available=true;}//liberation de la carte
+    public void setPlayer(Player p){player =p;}
 
 
 }
