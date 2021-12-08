421f3834afeb374ed27714bfda08f162b659b5b5
quentinBourdeau <quentin.bourdeau@etu.univ-cotedazur.fr>
2021-12-08 17:28:07 +0100

    choix du role HighScoreThief & ajout du role correspondant a l'enum district #17

--- src/main/java/fr/unice/polytech/startingpoint/buildings/District.java
+++ src/main/java/fr/unice/polytech/startingpoint/buildings/District.java
@@ -6,9 +6,19 @@ import fr.unice.polytech.startingpoint.characters.Character;
 
 public enum District {
 
-    Commercial,
-    Noble,
-    Military,
-    Prestige,
-    Religion
+    Commercial(5),
+    Noble(3),
+    Military(7),
+    Prestige(-1),
+    Religion(4);
+
+    private final int taxCollector;
+
+    District(int taxCollector) {
+        this.taxCollector = taxCollector;
+    }
+
+    public int getTaxCollector() {
+        return taxCollector;
+    }
 }
