d7822f91a4e80a8310e9e7280a03b4fb2f50068d
AmbreCorreia <74357454+AmbreCorreia@users.noreply.github.com>
2021-12-12 12:28:01 +0100

    BalancedFirst strategie #17

---
+++ src/main/java/fr/unice/polytech/startingpoint/strategies/BalancedFirst.java
@@ -0,0 +1,89 @@
+package fr.unice.polytech.startingpoint.strategies;
+
+import fr.unice.polytech.startingpoint.Board;
+import fr.unice.polytech.startingpoint.buildings.Building;
+import fr.unice.polytech.startingpoint.buildings.District;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
+import static java.util.Objects.isNull;
+
+public class BalancedFirst extends Player{
+    private final int costMin = 0;
+    private final int costMax = 6;
+
+    public BalancedFirst(Board b){super(b, "BalancedFisrt");}
+
+    @Override
+    public void roleEffects() {
+        if (getRole().isPresent()) {
+            getRole().get().usePower(board);
+        }
+    }
+
+    @Override
+    public List<Building> buildDecision() {
+        //Scale of cost ok for building
+        return buildDecision(costMin, costMax);
+    }
+
+    @Override
+    public List<Building> buildDecision(int costMin, int costMax) {
+        List<Building> checkBuilding = new ArrayList<>(getCardHand());
+        checkBuilding.sort(this);
+        return checkBuilding;
+    }
+
+    @Override
+    public void chooseRole() {
+        //taxes priority
+        TreeMap<District, Integer> taxmap = new TreeMap<>();
+        for (District d : District.values()) {
+            taxmap.put(d, 0);
+        }
+        for (Building b : getCity()) {
+            taxmap.put(b.getDistrict(), taxmap.get(b.getDistrict()) + 1);
+        }
+        taxmap.remove(District.Prestige);
+        taxmap.remove(District.Noble);
+        ArrayList<Integer> taxList = (ArrayList<Integer>) taxmap
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey)
+                .map(District::getTaxCollector)
+                .collect(Collectors.toList());
+
+        //prioritize King, Assassin
+        taxList.add(0, 3);
+        taxList.add(0, 0);
+        taxList.addAll(List.of(
+                1, 2, 6
+        ));
+
+        for (int elem : taxList) {
+            if (pickRole(elem)) {
+                return;
+            }
+        }
+    }
+
+    @Override
+    public int compare(Building b1, Building b2) {
+        //return -1 pour b1, 1 pour b2
+        if (isNull(b1))
+            return 1;
+        else if (isNull(b2))
+            return -1;
+        else if (getCardHand().contains(b1))
+            return 1;
+        else if (getCardHand().contains(b2))
+            return -1;
+        if (b1.getDistrict() == District.Noble) return -1;
+        else return 1;
+    }
+}
