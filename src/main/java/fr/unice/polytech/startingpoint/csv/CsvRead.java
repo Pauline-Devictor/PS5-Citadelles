0462051c1de0163c9812442807a6b91e35d93277
Pauline-Devictor <pauline.devictor@etu.unice.fr>
2022-01-04 15:47:32 +0100

    Methode de recuperation de donnees d'un fichier csv a partir du path #33

--- src/main/java/fr/unice/polytech/startingpoint/csv/CsvRead.java
+++ src/main/java/fr/unice/polytech/startingpoint/csv/CsvRead.java
@@ -3,19 +3,24 @@ package fr.unice.polytech.startingpoint.csv;
 import au.com.bytecode.opencsv.CSVReader;
 
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import static fr.unice.polytech.startingpoint.Game.LOGGER;
 
 public class CsvRead {
-    public void read() {
+    /**
+     *
+     * @param path
+     */
+    public void printCsv(String path) {
         //Build reader instance
         //Read stats.csv
         //Default seperator is comma
         //Default quote character is double quote
         //Start reading from line number 1 (line numbers start from zero)
         try {
-            CSVReader reader = new CSVReader(new FileReader("src/main/resources/save/stats.csv"), ';', '"', 0);
+            CSVReader reader = new CSVReader(new FileReader(path), ';', '"', 0);
 
             //Read CSV line by line and use the string array as you want
             String[] nextLine;
@@ -24,7 +29,35 @@ public class CsvRead {
                 System.out.println(Arrays.toString(nextLine));
             }
         } catch (Exception e) {
+            LOGGER.severe("Failed to print :" + e.getMessage());
+        }
+    }
+
+    /**
+     * Stock datas in variable data
+     * @param path
+     * @return data contain in target csv
+     */
+    //utilisable pour recuperer les donnes de results csv pour actualiser les stats
+    public ArrayList<String> readCsv(String path) {
+        ArrayList<String> data = new ArrayList<String>();
+        //Build reader instance
+        //Read stats.csv
+        //Default seperator is comma
+        //Default quote character is double quote
+        //Start reading from line number 1 (line numbers start from zero)
+        try {
+            CSVReader reader = new CSVReader(new FileReader(path), ';', '"', 0);
+            String[] nextLine;
+            while ((nextLine = reader.readNext()) != null) {
+                //Verifying the read data here
+                data.add(Arrays.toString(nextLine));
+
+            }
+            return data;
+        } catch (Exception e) {
             LOGGER.severe("Failed to read :" + e.getMessage());
         }
+        return null;
     }
 }
